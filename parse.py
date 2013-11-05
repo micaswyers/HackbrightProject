35ad228924ca5341c26841ff58de9e5347a759f4
Mica Love Swyers <Mica@Micas-MacBook-Air.local>
2013-11-05 12:45:59 -0800

    Experimented with HTMLParser

--- parse.py
+++ parse.py
@@ -1,28 +1,37 @@
 """
-Takes in HTML document
-returns text file
+Takes in XML blog document
+Splits into posts 
+Adds each post to a list
+Returns list
 """
 
-import sys, HTMLParser
+import sys
+from HTMLParser import HTMLParser
 from bs4 import BeautifulSoup
 
-def convert(some_HTML):
-    HTML_text = BeautifulSoup(some_HTML)
-    just_text = HTML_text.get_text()
-    return just_text
+class MyHTMLParser(HTMLParser):
+    def handle_start(self, tag, attrs):
+        print "Encountered start tag: ", tag
+    def handle_endtag(self, tag):
+        print "Encountered end tag: ", tag
+    def handle_data(self, data):
+        print "Encountered some data: ", data
+
+parser = MyHTMLParser()
+
+
+
+def convert(input_text):
+    text = BeautifulSoup(input_text)
 
 
 def main():
-    script, input_text, output_text = sys.argv
+    script, input_text = sys.argv
 
     open_file = open(input_text, 'rb')
     input_text = open_file.read()
     open_file.close()
 
-    just_text = convert(input_text)
-
-    text_file = open(output_text, 'wb')
-    text_file.write(just_text.encode("utf-8"))
-    text_file.close()
+    parser.feed(input_text)
 
 main()
