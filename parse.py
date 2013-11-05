8cbabc7e92756a7c1cfbc5bfaa1ff87e14674a94
Mica Love Swyers <Mica@Micas-MacBook-Air.local>
2013-11-05 15:35:28 -0800

    Now adds blog posts to a list

--- parse.py
+++ parse.py
@@ -2,25 +2,37 @@
 Takes in XML blog document
 Splits into posts 
 Adds each post to a list
-Returns list
+
+
+
 """
 
 import sys
 from HTMLParser import HTMLParser
 from bs4 import BeautifulSoup
 
+
 class MyHTMLParser(HTMLParser):
-    def handle_start(self, tag, attrs):
-        print "Encountered start tag: ", tag
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.post = False
+        self.post_list = []
+    def handle_starttag(self, tag, attrs):
+        print tag
+        if tag == "post":
+            self.post = True
     def handle_endtag(self, tag):
-        print "Encountered end tag: ", tag
+        self.post = False
     def handle_data(self, data):
-        print "Encountered some data: ", data
+        if self.post == True:
+            print data
+            self.post_list.append(data)
+
 
 parser = MyHTMLParser()
 
 
-
+#Not actually using this right now
 def convert(input_text):
     text = BeautifulSoup(input_text)
 
@@ -33,5 +45,6 @@ def main():
     open_file.close()
 
     parser.feed(input_text)
+    print parser.post_list
 
 main()
