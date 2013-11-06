c8dd7c6d8d4e1f5054b8a4cd7cb10e80a243576a
Mica Love Swyers <Mica@Micas-MacBook-Air.local>
2013-11-05 16:03:07 -0800

    Removes leading and trailing escapes

--- parse.py
+++ parse.py
@@ -3,8 +3,9 @@ Takes in XML blog document
 Splits into posts 
 Adds each post to a list
 
-
-
+Cleans each post (removes escapes)
+Assigns (x,y) to each post (word length, sentence length)
+Run k-means algorithm on all blog scores to give average? (Or just average)
 """
 
 import sys
@@ -18,14 +19,12 @@ class MyHTMLParser(HTMLParser):
         self.post = False
         self.post_list = []
     def handle_starttag(self, tag, attrs):
-        print tag
         if tag == "post":
             self.post = True
     def handle_endtag(self, tag):
         self.post = False
     def handle_data(self, data):
         if self.post == True:
-            print data
             self.post_list.append(data)
 
 
@@ -36,6 +35,10 @@ parser = MyHTMLParser()
 def convert(input_text):
     text = BeautifulSoup(input_text)
 
+def normalize(input_text):
+    input_text = input_text.strip()
+    return input_text
+
 
 def main():
     script, input_text = sys.argv
@@ -45,6 +48,15 @@ def main():
     open_file.close()
 
     parser.feed(input_text)
-    print parser.post_list
+    
+    post_list = parser.post_list
+    one_post = normalize(post_list[0])
+    print "%r" % one_post
 
+
+    counter = 0
+    for post in post_list:
+        post = normalize(post)
+        print "Post #%d: " %counter, post
+        counter += 1
 main()
