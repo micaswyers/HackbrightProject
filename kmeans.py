8c2e81a9584f663c6bdc159ec34162790bb10867
Mica Love Swyers <Mica@Micas-MacBook-Air.local>
2013-11-11 15:59:54 -0800

    Clustering now works

--- kmeans.py
+++ kmeans.py
@@ -5,21 +5,28 @@ clusters into k groups
 """
 
 import sys
+from numpy import array
 from scipy.cluster.vq import whiten, vq, kmeans, kmeans2
 
 def get_vectors(input = sys.stdin):
     vectors = []
     for line in input:
-        vectors.append(eval(line))
+        vectors.append(array(eval(line)))
     return vectors
 
 def main(input): 
     whitened = whiten(obs=get_vectors(input))
-    clusters = kmeans(whitened, 2)
-    print clusters
+    code_book = kmeans(whitened, 3) #returns tuple (codebook, distortion)
+    clustered_results = vq(whitened, code_book[0])
+    print clustered_results
 
-# main()
-print "sys.argv: ", sys.argv
+# # This code prints out the clustering results to the CL
+#     cluster_list = map(None, clustered_results[0])
+#     counter = 1
+#     for centroid in cluster_list:
+#         print "Post #%d belongs in Cluster #%d" % (counter, centroid)
+#         counter += 1
+
 if len(sys.argv) > 1:
     for pathname in sys.argv[1:]:
         pathname = open(pathname)
