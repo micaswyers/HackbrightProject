80556b1c9cd39bbcb5f8c831c50f4ee1c0b3c0d6
Mica Love Swyers <Mica@Micas-MacBook-Air.local>
2013-11-14 15:51:37 -0800

    Working through JSON

--- static/myjs.js
+++ static/myjs.js
@@ -2,3 +2,15 @@
  $(document).ready(function() {
                 console.log('ready!');
             });
+
+ $("#submit").click(function (e) {
+    console.log("User clicked submit button.");
+    e.preventDefault(); //Overrides submit button defaults
+    var input = $("#text_area_sample").val();
+    $.get("/butts", {'words': input}, function (response) {
+        $("#sample_input").html(" ");
+        var obj = $.parseJSON(response);
+        console.log(obj);
+        $("#sample_input").append(obj[0].post);
+    });
+});
