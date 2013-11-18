5372c4a927a9a8ad407311dbeff42eb8742cd784
Mica Love Swyers <Mica@Micas-MacBook-Air.local>
2013-11-18 15:18:26 -0800

    Deleted parent directory from filenames

--- test.py
+++ test.py
@@ -2,16 +2,24 @@ import sys, csv
 
 def make_filenames_list():
     filenames = []
+
     for pathname in sys.argv[1:]:
-        filenames.append([pathname])
+        index = pathname.rfind("/")
+        shortened_pathname = pathname[index+1:]
+        filenames.append(shortened_pathname)
+
     return filenames
 
-def print_to_csv(filenames_list):
+def print_filenames_to_csv(filenames_list):
     f = open('blog_info.csv', 'wb')
-    writer=csv.writer(f)
-    writer.writerows(filenames_list)
+    writer=csv.writer(f, delimiter = "\n")
+    writer.writerow(filenames_list)
 
 def main():
-    print_to_csv(make_filenames_list())
+    print_filenames_to_csv(make_filenames_list())
+    
+def print_posts_to_csv(posts_list):
+    pass
+    
 
 main()
