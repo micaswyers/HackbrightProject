4b4a5ea95e3cdd961b2e942d3d141afa5b8a570f
Mica Love Swyers <Mica@Micas-MacBook-Air.local>
2013-11-19 11:26:41 -0800

    Working with JS

--- static/myjs.js
+++ static/myjs.js
@@ -6,18 +6,22 @@
 
  $("#submit").click(function (e) { //what does the e do again?
     console.log("User clicked submit button.");
-    e.preventDefault(); //Overrides submit button defaults
+    e.preventDefault(); //Overrides submit button defaults or return False prevents event bubbling
     $("#search_results").show();
     var input=$("#text_area_sample").val();
 
+//.get == .ajax 
     // $.get("/butts", {'words': input}, function (response) {
     //     var returned_dictionary = $.parseJSON(response);
     //     $("#search_results").html("\"" + returned_dictionary[0].title + ": \n" +returned_dictionary[0].post + "\"");
     // });
 
+
+//before send to show loading behavior; add loading here 
     $.ajax({
         url:"/butts",
-        data: input
+        data: input,
+        // beforeSend: (function() {$("#search_results").show();})
     }).done(function(response) {
         console.log("The response came back");
         var returned_dictionary = $.parseJSON(response);
