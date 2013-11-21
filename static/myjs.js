6b3f5eb610dd5b5e8ad953ebe182ed20f1fb9d21
Mica Love Swyers <Mica@Micas-MacBook-Air.local>
2013-11-20 17:28:37 -0800

    Playing around with design-ish things. Oh geezus. Why is it all ugly?

--- static/myjs.js
+++ static/myjs.js
@@ -1,13 +1,13 @@
  
  $(document).ready(function() {
     console.log('ready!');
-    $("#search_results").hide();
+    $(".loading").hide();
     });
 
  $("#submit").click(function (e) {
     console.log("User clicked submit button.");
     e.preventDefault(); //Overrides submit button defaults or return False prevents event bubbling
-    $("#search_results").show();
+    $(".loading").show(4000);
     var input=$("#text_area_sample").val();
 
     $.ajax({
@@ -17,7 +17,8 @@
         console.log("The response came back");
         var returned_dictionary = $.parseJSON(response);
         console.log(returned_dictionary);
-        $("#loading").remove();
-        $("#text").html(returned_dictionary[0].post);
+        $(".loading").hide();
+        $("#search_results").html('<p class="big-text">' + "Based on your input text, you should read: " +  '</p>');
+        $("#search_results").append('<p class="text">' + returned_dictionary[0].post + '</p>');
     });
 });
