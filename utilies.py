74d5b91e33f02e339faf871f68fd828c11b1a2c3
Mica Love Swyers <Mica@Micas-MacBook-Air.local>
2013-11-20 18:58:42 -0800

    Removed utilies

--- utilies.py
+++
@@ -1,9 +0,0 @@
-def normalize(text):
-    mapping = [ (u"\u2018", "'"),
-                (u"\u2019", "'") ]
-    new_text = text
-
-    for src, dst in mapping:
-        new_text = new_text.replace(src, dst)
-   
-    return new_text.encode ("utf8")
