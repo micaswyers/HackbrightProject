e00aa6247819d06c589ef041a6417fd9fa278188
Mica Love Swyers <Mica@Micas-MacBook-Air.local>
2013-11-22 11:17:13 -0800

    Loading icon now displays every time user hits SUBMIT button

--- static/myjs.js
+++ static/myjs.js
@@ -7,7 +7,8 @@
  $("#submit").click(function (e) {
     console.log("User clicked submit button.");
     e.preventDefault(); //Overrides submit button defaults or return False prevents event bubbling
-    $(".loading").show(4000);
+    $(".loading").show();
+    $(".search_results").html(" ");
     var input=$("#text_area_sample").val();
 
     $.ajax({
@@ -18,16 +19,16 @@
         var returned_dictionary = $.parseJSON(response);
         console.log(returned_dictionary);
         $(".loading").hide();
-        $("#search_results").html('<p class="big-text">' + "Input text stats: " +  '</p>');
-        $("#search_results").append('<p class="text">' + returned_dictionary[0].sample_feature_vector[0] + "Total words" + '</p>');
-        $("#search_results").append('<p class="text">' + returned_dictionary[0].sample_feature_vector[1] + "1st-person singular pronouns" + '</p>');
-        $("#search_results").append('<p class="text">' + returned_dictionary[0].sample_feature_vector[2] + "exclamation points" + '</p>');
-        $("#search_results").append('<p class="text">' + returned_dictionary[0].sample_feature_vector[3] + " average words/sentence" + '</p>');
-        $("#search_results").append('<p class="text"' + returned_dictionary[0].     post_feature_vector);
-        $("#search_results").append('<p class="big-text">' + "You should read: " +  '</p>');
-        $("#search_results").append('<p class="text">' + "Post ID#: " + returned_dictionary[0].id + '</p>');
-        $("#search_results").append('<p class="text">' + "Cluster ID#: " + returned_dictionary[0].cluster + '</p>');
-        $("#search_results").append('<p class="text">' + "Feature Vector: " + returned_dictionary[0].post_feature_vector + '</p>');
-        $("#search_results").append('<p class="text">' + returned_dictionary[0].text + '</p>');
+        $(".search_results").append('<p class="big-text">' + "Input text stats: " +  '</p>');
+        $(".search_results").append('<p class="text">' + returned_dictionary[0].sample_feature_vector[0] + "Total words" + '</p>');
+        $(".search_results").append('<p class="text">' + returned_dictionary[0].sample_feature_vector[1] + "1st-person singular pronouns" + '</p>');
+        $(".search_results").append('<p class="text">' + returned_dictionary[0].sample_feature_vector[2] + "exclamation points" + '</p>');
+        $(".search_results").append('<p class="text">' + returned_dictionary[0].sample_feature_vector[3] + " average words/sentence" + '</p>');
+        $(".search_results").append('<p class="text"' + returned_dictionary[0].     post_feature_vector);
+        $(".search_results").append('<p class="big-text">' + "You should read: " +  '</p>');
+        $(".search_results").append('<p class="text">' + "Post ID#: " + returned_dictionary[0].id + '</p>');
+        $(".search_results").append('<p class="text">' + "Cluster ID#: " + returned_dictionary[0].cluster + '</p>');
+        $(".search_results").append('<p class="text">' + "Feature Vector: " + returned_dictionary[0].post_feature_vector + '</p>');
+        $(".search_results").append('<p class="text">' + returned_dictionary[0].text + '</p>');
     });
 });
