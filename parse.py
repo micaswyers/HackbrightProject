dd88070803df40660ee29efe3160ae68f98f743b
Mica Love Swyers <Mica@Micas-MacBook-Air.local>
2013-11-25 11:06:30 -0800

    Cleaned up function

--- parse.py
+++ parse.py
@@ -6,9 +6,10 @@ FIRST_PERSON_SINGULAR_PRONOUNS = set(("i", "i'm", "i've", "i'll", "i'd", "me", "
 
 def calculate_feature_vector(post):
     exclamation_count = count_punctuation(post)
-    words = make_wordcount_dict2(post)
-    I_count = count_i(words)
     average_sentence_length = find_average_sentence_length(post)
+
+    words = make_wordcount_dict(post)
+    I_count = count_i(words)
 
     total_words = 0
     for word in words:
@@ -25,16 +26,16 @@ def comparator(x,y):
     else:
         return cmp(x[0], y[0])
 
+def count_i(words_dict):
+    total = 0
+    for word in words_dict:
+        if word in FIRST_PERSON_SINGULAR_PRONOUNS:
+            total += words_dict[word]
+    return total 
+
 def count_punctuation(sample):
     ep_matches = re.findall("!", sample)
     return len(ep_matches)
-
-def count_i(words):
-    total = 0
-    for word in words:
-        if word in FIRST_PERSON_SINGULAR_PRONOUNS:
-            total += words[word]
-    return total 
 
 def find_average_sentence_length(sample):
     #regular expressions for lexical analysis
@@ -55,18 +56,37 @@ def find_average_sentence_length(sample):
         average_sentence_length = total_words/len(sentences)
     return average_sentence_length
 
-def make_wordcount_dict2(post):
+def make_wordcount_dict(post):
     token_list = normalize(post)
     wordcount = {}
     for token in token_list:
         wordcount[token] = wordcount.get(token, 0) + 1
     return wordcount
 
+def normalize(text): 
+    text = text.decode("utf8").replace(u"\u2014", " ")
+    word_list = text.lower().split()
+    
+    clean_list = []
+    for word in word_list:
+        word = word.strip("?.,_;\":!'-")
+        clean_list.append(word)
+    return clean_list
+
 def open_file(input_blog):
     f = open(input_blog, 'rb')
     input_blog = f.read()
     f.close()
     return input_blog
+
+def process_one_blog(filename):
+    list_of_posts = separate_posts(filename)
+
+    for post in list_of_posts:
+        feature_vector = calculate_feature_vector(post)
+        index = filename.rfind("/")
+        shortened_filename = filename[index+1:]
+        print repr((feature_vector, shortened_filename, post))
 
 def print_by_frequency(words):
     for k in sorted(words.iteritems(), cmp=comparator):
@@ -82,25 +102,6 @@ def separate_posts(input_blog):
         post = section.contents[0].strip()
         post_list.append(post)
     return post_list
-
-def normalize(text): 
-    text = text.decode("utf8").replace(u"\u2014", " ")
-    word_list = text.lower().split()
-    
-    clean_list = []
-    for word in word_list:
-        word = word.strip("?.,_;\":!'-")
-        clean_list.append(word)
-    return clean_list
-
-def process_one_blog(filename):
-    list_of_posts = separate_posts(filename)
-
-    for post in list_of_posts:
-        feature_vector = calculate_feature_vector(post)
-        index = filename.rfind("/")
-        shortened_filename = filename[index+1:]
-        print repr((feature_vector, shortened_filename, post))
 
 if __name__ == "__main__":
     for pathname in sys.argv[1:]:
